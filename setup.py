adb03acad4a5bb79c7a31ae5db0884c411d83b54
Adam Newgas <boris@boristhebrave.com>
2015-08-27 21:14:54 +0100

    Packaging.

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+    name='axaxaxas',
+    version="1.0.0",
+    description="A simple to use Earley Parser",
+    long_description="""Axaxaxas is a full Earley Parser with an emphasis on ease of use and customization.
+    Earley parsers are a type of chart parser with robust support for ambiguous syntax.""",
+    author="Adam Newgas",
+    license="MIT",
+    classifiers=[
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Developers',
+    'Topic :: Text Processing :: General',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.3',
+        ],
+    keywords='earley parser natural language',
+    packages=["axaxaxas"],
+)
+
